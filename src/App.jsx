52278448f8b7eb95a2cba4800b701de149988aed
Balevicky <balevictorien@yahoo.fr>
2024-08-17 10:17:05 +0000

    Added FetchYogaDate.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import FetchData from './Components/FetchData'
-
+import FetchYogaData from './Components/FetchYogaData'
 function App() {
   return (
  <>
+ <p>
  <FetchData/>
+ </p>
+ <p>
+ <FetchYogaData/>
+ </p>
  </>
   )
 }
